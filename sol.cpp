554c44d49c37b076e38ce4665e3f8516cd33b24a
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-03-28 20:42:57 +0800

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+void perform_test() {
+	string s;
+	cin >> s;
+	// the answer always exists as long as binary representations
+	// of 2 and 3 are present in the given string
+	bool checker = false;
+	for(int i = 0; i < (int) s.size(); i++) {
+		string temp = s.substr(i, 2);
+		checker |= (temp == "10" || temp == "11");
+	}
+	cout << (checker ? "Yes" : "No") << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tc = 1;
+	cin >> tc;
+	for(int t = 0; t < tc; t++) {
+		perform_test();
+	}
+	return 0;
+}
